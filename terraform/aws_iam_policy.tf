7a22d6eddb0acfdf6f9fb6a3f0e989e72e9f74f4
k2216443 <k2216443@gmail.com>
2025-10-13 19:47:31 +0200

    Message To TG

--- terraform/aws_iam_policy.tf
+++ terraform/aws_iam_policy.tf
@@ -1,67 +1,134 @@
 resource "aws_iam_policy" "codebuild" {
-  name   = "${var.name}-codebuild-policy"
-  policy = data.aws_iam_policy_document.codebuild_policy.json
+  name = "${var.name}-codebuild-policy"
+  policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action   = "ecr:GetAuthorizationToken"
+          Effect   = "Allow"
+          Resource = "*"
+          Sid      = "ECRAuth"
+        },
+        {
+          Action = [
+            "ssm:GetParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParameter",
+          ]
+          Effect = "Allow"
+          Resource = [
+            "arn:aws:ssm:us-west-2:292875404443:parameter/cryptoeye/telegram_chat_id",
+            "arn:aws:ssm:us-west-2:292875404443:parameter/cryptoeye/telegram_bot_token",
+          ]
+        },
+        {
+          Action = [
+            "lambda:UpdateFunctionCode",
+            "lambda:GetFunctionConfiguration",
+          ]
+          Effect   = "Allow"
+          Resource = "arn:aws:lambda:us-west-2:292875404443:function:cryptoeye"
+          Sid      = "LambdaUpdate"
+        },
+        {
+          Action = [
+            "ecr:UploadLayerPart",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:DescribeRepositories",
+            "ecr:CompleteLayerUpload",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+          ]
+          Effect = "Allow"
+          Resource = [
+            "arn:aws:ecr:us-west-2:292875404443:repository/python",
+            "arn:aws:ecr:us-west-2:292875404443:repository/cryptoeye",
+          ]
+          Sid = "ECRPush"
+        },
+        {
+          Action = [
+            "logs:PutLogEvents",
+            "logs:CreateLogStream",
+            "logs:CreateLogGroup",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+          Sid      = "Logs"
+        },
+        {
+          Action   = "sts:GetCallerIdentity"
+          Effect   = "Allow"
+          Resource = "*"
+          Sid      = "STSRead"
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
 }
 
-data "aws_iam_policy_document" "codebuild_policy" {
-  statement {
-    sid       = "ECRAuth"
-    effect    = "Allow"
-    actions   = ["ecr:GetAuthorizationToken"]
-    resources = ["*"]
-  }
-    statement {
-    effect = "Allow"
-    actions = ["ssm:GetParameter", "ssm:GetParameters", "ssm:GetParameterHistory"]
-    resources = [
-      aws_ssm_parameter.tg_bot_token.arn,
-      aws_ssm_parameter.tg_chat_id.arn
-    ]
-  }
-  statement {
-    sid    = "LambdaUpdate"
-    effect = "Allow"
-    actions = [
-      "lambda:UpdateFunctionCode",
-      "lambda:GetFunctionConfiguration"
-    ]
-    resources = [aws_lambda_function.fn.arn]
-  }
-  statement {
-    sid    = "ECRPush"
-    effect = "Allow"
-    actions = [
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:CompleteLayerUpload",
-      "ecr:InitiateLayerUpload",
-      "ecr:PutImage",
-      "ecr:UploadLayerPart",
-      "ecr:BatchGetImage",
-      "ecr:DescribeRepositories",
-      "ecr:GetDownloadUrlForLayer"
-    ]
-    resources = [
-      "${aws_ecr_repository.app.arn}",
-      "${aws_ecr_repository.python.arn}",
-    ]
-  }
-  statement {
-    sid    = "Logs"
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = ["*"]
-  }
-  statement {
-    sid       = "STSRead"
-    effect    = "Allow"
-    actions   = ["sts:GetCallerIdentity"]
-    resources = ["*"]
-  }
-}
+# data "aws_iam_policy_document" "codebuild_policy" {
+#   statement {
+#     sid       = "ECRAuth"
+#     effect    = "Allow"
+#     actions   = ["ecr:GetAuthorizationToken"]
+#     resources = ["*"]
+#   }
+#     statement {
+#     effect = "Allow"
+#     actions = ["ssm:GetParameter", "ssm:GetParameters", "ssm:GetParameterHistory"]
+#     resources = [
+#       aws_ssm_parameter.tg_bot_token.arn,
+#       aws_ssm_parameter.tg_chat_id.arn
+#     ]
+#   }
+#   statement {
+#     sid    = "LambdaUpdate"
+#     effect = "Allow"
+#     actions = [
+#       "lambda:UpdateFunctionCode",
+#       "lambda:GetFunctionConfiguration"
+#     ]
+#     resources = [aws_lambda_function.fn.arn]
+#   }
+#   statement {
+#     sid    = "ECRPush"
+#     effect = "Allow"
+#     actions = [
+#       "ecr:BatchCheckLayerAvailability",
+#       "ecr:CompleteLayerUpload",
+#       "ecr:InitiateLayerUpload",
+#       "ecr:PutImage",
+#       "ecr:UploadLayerPart",
+#       "ecr:BatchGetImage",
+#       "ecr:DescribeRepositories",
+#       "ecr:GetDownloadUrlForLayer"
+#     ]
+#     resources = [
+#       "${aws_ecr_repository.app.arn}",
+#       "${aws_ecr_repository.python.arn}",
+#     ]
+#   }
+#   statement {
+#     sid    = "Logs"
+#     effect = "Allow"
+#     actions = [
+#       "logs:CreateLogGroup",
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents"
+#     ]
+#     resources = ["*"]
+#   }
+#   statement {
+#     sid       = "STSRead"
+#     effect    = "Allow"
+#     actions   = ["sts:GetCallerIdentity"]
+#     resources = ["*"]
+#   }
+# }
 
 resource "aws_iam_policy" "lambda_ecr_pull" {
   name   = "${var.name}-ecr-pull"
--- terraform/aws_lambda_function.tf
+++ terraform/aws_lambda_function.tf
@@ -6,10 +6,12 @@ resource "aws_lambda_function" "fn" {
   image_uri    = var.image_uri
 
   architectures = ["x86_64"]
-  memory_size   = "512"
-  timeout       = "30"
+  memory_size   = "64"
+  timeout       = "10"
 
   depends_on = [aws_iam_role.lambda_role]
 
-  
+  lifecycle {
+    ignore_changes = [image_uri]
+  }
 }
